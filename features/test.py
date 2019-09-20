37d1059bf61cc49ad046835db981592c57465b4a
Matthew Fielding <mattfielding8@gmail.com>
2019-09-20 08:45:10 +0100

    Cleans test code -> commented code removed

--- features/test.py
+++ features/test.py
@@ -12,38 +12,12 @@ y = np.zeros(10)
 y[temp] = 1
 y = y.reshape(10,1)
 
-# print(y.shape)
-
-# print(X)
-# print(X.shape)
-
 net = mod.NeuralNetwork([784,16,16,10])
 
+a = net.forwardProp(X)
 b = net.backProp(X, y)
 
 print("Bias length", [i.shape for i in net.biasses])
 print("Weights length", [i.shape for i in net.weights])
+print("Grads", [i.shape for i in b])
 
-print(b)
-
-# a = net.forwardProp(X)
-
-
-# print(net.activations)
-
-# print(a.shape)
-# print([i.shape for i in net.activations])
-# print(a)
-
-# a = np.array([[0,1],[2,3]])
-# b = np.array([[1],[1]])
-
-# print(a)
-# print(b)
-# print(a+b)
-
-# print((np.dot(net.weights[0], X).T + net.biasses[0]))
-# print(np.dot(net.weights[0], X).shape)
-# print(net.biasses[0].shape)
-#
-# print(list(reversed(enumerate(np.ndarray([4,1])))))
