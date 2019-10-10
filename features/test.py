11a4adf6306b8f337b66419fb185683d35ca4433
Matthew Fielding <mattfielding8@gmail.com>
2019-10-10 07:44:17 +0100

    Tests fit() function in NN class -> NN is learning

--- features/test.py
+++ features/test.py
@@ -6,18 +6,19 @@ df = pd.read_csv('../data/train.csv', nrows=100)
 print(df.shape)
 print(df.head())
 
-X = df.iloc[1,1:].values.reshape(784,1)
-temp = df.iloc[1,0]
-y = np.zeros(10)
-y[temp] = 1
-y = y.reshape(10,1)
+X = df.iloc[:,1:].values
+X = X/255
+print(X.max())
+y = df.iloc[:,0]
+y = mod.y_binary(y)
 
-net = mod.NeuralNetwork([784,16,16,10])
+net = mod.NeuralNetwork([784,784,10])
 
-a = net.forwardProp(X)
-b = net.backProp(X, y)
+net.fit(X,y,epochs=5,batch_size=1)
 
-print("Bias length", [i.shape for i in net.biasses])
-print("Weights length", [i.shape for i in net.weights])
-print("Grads", [i.shape for i in b])
+a, zs = net.forwardProp(X)
+
+print("a", a)
+print("zs", zs)
+print("sigs", [mod.sigmoid(z) for z in zs])
 
